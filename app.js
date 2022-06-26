692db4a1ada1084c291ba4a09a1b1e07d4c29f31
tbrown034 <tbrown034@gmail.com>
2022-06-25 23:41:33 -0500

    board is set working on js now

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+const playerX = 'x'
+const playerO ='o'
+
+
+const cells = document.querySelectorAll(".cell");
+
+const winningMessage = document.querySelectorAll("#winningMessageText");
+
+const restartButton = document.querySelectorAll("#restartButton");
+
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+let moves = ["", "", "", "", "", "", "", "", ""]
+
+let PlayerOneName ='X'
+let PlayerTwoName ='0'
+let activeGame = false;
+
 const gameState = {
     players: ['x', 'o'],
     board: [
@@ -6,4 +33,3 @@ const gameState = {
       [null, null, null]
     ]
   }
-  dfasdf
