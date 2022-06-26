2811c30c1dc984a04290c5f1c79a64182effcfa0
tbrown034 <tbrown034@gmail.com>
2022-06-26 15:08:31 -0500

    stuck at 3pm

--- app.js
+++ app.js
@@ -1,72 +1,22 @@
-const playerX = 'x'
-const playerO ='o'
+const cellElements = document.querySelectorAll('.cell');
+const board = document.getElementById('board')
+// Pull in the result text from DOM
+const resultElement = document.getElementById('result');
 
+// Add event listener
+cellElements.forEach((cell, index) => {
+  cell.addEventListener("click", () => {
+    placeMarker(index);
+  });
+});
 
-const cells = document.querySelectorAll(".cell");
-
-const winningMessage = document.querySelectorAll("#winningMessageText");
-
-const restartButton = document.querySelectorAll("#restartButton");
-
-const winConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
-
-let moves = ["", "", "", "", "", "", "", "", ""]
-
-let PlayerOneName ='x'
-let PlayerTwoName ='0'
-let activeGame = false;
-
-const gameState = {
-    players: ['x', 'o'],
-    board: [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null]
-    ]
-  }
-
-// Functions //
-
-function GetInputValue1 (){
-  var inputValue1 = document.getElementsbyId("PlayerOneName").value;
-  alert(inputValue1);
+ 
+function nameOneFunc() {
+  var onefunc = document.getElementById("myText").value;
+  document.getElementById("play1name").innerHTML = onefunc;
 }
 
-function howManyPlayers(){
-
-}
-
-
-function startGame (){
-  cells.forEach()
-
-}
-
-function clickCell(){
-
-}
-
-function updateBoard(cell, index){
-
-}
-
-function changePlayer(){
-
-}
-
-function CheckWin(){
-
-}
-
-function reseteGame(){
-
+function nameTwoFunc() {
+  var twofunc = document.getElementById("myText2").value;
+  document.getElementById("play2name").innerHTML = twofunc;
 }
